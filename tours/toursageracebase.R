b4e3300d56dde8ab6ed44bacf8cb658e4330ec61
Minzhao Liu <liuminzhao@gmail.com>
2013-09-06 14:43:25 -0500

    plot in tours age race base

--- tours/toursageracebase.R
+++ tours/toursageracebase.R
@@ -1,5 +1,5 @@
 #!/bin/Rscript
-##' Time-stamp: <liuminzhao 08/31/2013 14:58:01>
+##' Time-stamp: <liuminzhao 09/01/2013 15:48:44>
 ##' manipulate data TOURS
 ##' 2013/06/05 focus on AGE and RACE
 ##' 2013/06/22 add baseline y0 as a covariate
@@ -7,20 +7,22 @@
 
 rm(list=ls())
 library(bqrpt)
+library(quantreg)
+source('../R/BQRboot.R')
 
 TOURS <- read.csv('~/Documents/qr-draft/tours/tours.csv')
 
 TOURS <- subset(TOURS, RACE==1 | RACE==3)
 
-weight1 <- TOURS$wtkg1
-weight2 <- TOURS$wtkg2
-weight3 <- TOURS$wtkg3
+weight1 <- TOURS$wtkg1/100
+weight2 <- TOURS$wtkg2/100
+weight3 <- TOURS$wtkg3/100
 age <- TOURS$AGE
 trt <- TOURS$TREATMENT
 age_center <- (age-50)/5
 race3 <- as.numeric(TOURS$RACE == 3)
 
-y <- weight2 - weight1
+y <- (weight2 - weight1)*10
 n <- length(y)
 X <- matrix(0, n, 4)
 X[,1] <- 1
@@ -28,35 +30,33 @@ X[,2] <- age_center
 X[,3] <- race3
 X[,4] <- weight1
 
-dat <- data.frame(weight2, weight3, trt, age_center, age=TOURS$AGE, race = factor(TOURS$RACE), base = weight1)
+dat <- data.frame(weight1, weight2, weight3, trt, age_center, age=TOURS$AGE, race = factor(TOURS$RACE), base = weight1)
 
 ## PLOT
-## library(ggplot2)
-## library(gridExtra)
-## ggplot(data = dat, aes(x = age_center, y = weight2)) + geom_point()
-## ggplot(data = dat, aes(x = age_center, y = weight3)) + geom_point()
+library(ggplot2)
+library(gridExtra)
+ggplot(data = dat, aes(x = age_center, y = y)) + geom_point()
 
-## box1 <- ggplot(data = dat, aes(x = age, y = weight2*100)) + geom_point()+ ylab('Weight (Kg) at 6 months') + xlab('Age')
-## box2 <- ggplot(data = dat, aes(x = age, y = weight3*100)) + geom_point()+ ylab('Weight (Kg) at 18 months') + xlab('Age')
-## box3 <- ggplot(data = dat, aes(x = race, y = weight2*100)) + geom_boxplot() + scale_x_discrete(labels=c("Black", "White")) + ylab('Weight (Kg) at 6 months') + xlab('Race')
-## box4 <- ggplot(data = dat, aes(race, y = weight3*100)) + geom_boxplot() + ylab('Weight (Kg) at 18 months') + xlab('Race') + scale_x_discrete(labels=c("Black", "White"))
-## box5 <- ggplot(data = dat, aes(x = weight1*100, y = weight2*100)) + geom_point() + ylab('Weight (Kg) at 6 months') + xlab('Weight (Kg) at baseline')
-## box6 <- ggplot(data = dat, aes(x = weight1*100, y = weight3*100)) + geom_point() + ylab('Weight (Kg) at 18 months') + xlab('Weight (Kg) at baseline')
+ggplot(data = dat, aes(x = weight1, y = y)) + geom_point()
 
-## scat1 <- ggplot(data = dat, aes(x = age_center, y = weight2, color = race)) + geom_point()
-## scat2 <- ggplot(data = dat, aes(x = age_center, y = weight3, color = race)) + geom_point()
-## scat3 <- ggplot(data = dat, aes(x = age_center, y = weight2, color = race)) + geom_point()
-## scat4 <- ggplot(data = dat, aes(x = age_center, y = weight3, color = race)) + geom_point()
+ggplot(data = dat, aes(x = race, y = y)) + geom_boxplot() + scale_x_discrete(labels=c("Black", "White")) + ylab('Weight (Kg) at 6 months') + xlab('Race')
 
-## pdf('weight-age-race-base.pdf')
-## sds <- grid.arrange(box1, box2, box3, box4, box5, box6, nrow = 2, ncol = 3)
-## dev.off()
+ggplot(data = dat, aes(x = age_center, y = y, color = race)) + geom_point()
+
+box3 <- ggplot(data = dat, aes(x = race, y = y)) + geom_boxplot() + scale_x_discrete(labels=c("Black", "White")) + ylab('Weight (Kg) at 6 months') + xlab('Race')
+
+scat <- ggplot(data = dat, aes(x = age_center, y = y, color = race)) + geom_point()
+
+pdf('weight-age-race-base.pdf')
+sds <- grid.arrange(box1, box2, box3, box4, box5, box6, nrow = 2, ncol = 3)
+dev.off()
 
 ###############
 ## ANALYSIS
 ###############
 
-mcmc <- list(nburn=10000, nskip=1, nsave=10000, ndisp=10000, arate=0.25)
+mcmc <- list(nburn=30000, nskip=2, nsave=10000, ndisp=10000, arate=0.2)
+## mcmc <- list(nburn=30000, nskip=1, nsave=30000, ndisp=10000, arate=0.25)
 quan <- c(0.1, 0.3, 0.5, 0.7, 0.9)
 
 modss <- HeterPTlm(y, X, mcmc = mcmc, quan = quan, prior = list(maxm = 6), den = TRUE, method = 'ss')
@@ -65,13 +65,17 @@ mod <- HeterPTlm(y, X, mcmc = mcmc, quan = quan, prior = list(maxm = 6), den = T
 
 summary(mod)
 summary(modss)
-## age race
 
-X <- X[, 1:3]
+## RQ
 
-mod3ss <- HeterPTlm(y, X, mcmc = mcmc, quan = quan, prior = list(maxm = 6), den = TRUE, method = 'ss')
+modrq <- rq(y ~ age_center + race3, tau = c(0.1,0.3,0.5,0.7,0.9))
 
-mod3 <- HeterPTlm(y, X, mcmc = mcmc, quan = quan, prior = list(maxm = 6), den = TRUE, method = 'normal')
+## BQR
+modbqr1 <- BayesQReg(y, X, 0.1)
+modbqr3 <- BayesQReg(y, X, 0.3)
+modbqr5 <- BayesQReg(y, X, 0.5)
+modbqr7 <- BayesQReg(y, X, 0.7)
+modbqr9 <- BayesQReg(y, X, 0.9)
 
-summary(mod3)
-summary(mod3ss)
+BQRCoef(modbqr1)
+BQRCI(modbqr1)
